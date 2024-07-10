337703282bb1ceb8de4471cfd78b2129c1e447bb
LeonMadsen <140187535+LeonMadsen@users.noreply.github.com>
2024-07-10 16:37:33 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -42,31 +42,15 @@ sliderDots.forEach((dot, index) => {
     })
 })
 
-
-document.querySelectorAll('.category').forEach(item => {
-    item.addEventListener('click', function() {
-        document.querySelectorAll('.category').forEach(item => {
-            item.classList.remove('sidebar-active');
-            item.classList.add('sidebar-txt');
-        });
-        this.classList.remove('sidebar-txt');
-        this.classList.add('sidebar-active');
-    });
-});
-
-
-
-
-// блок с подгрузкой книг по API
 function fetchBooks() {
     const apiKey = "AIzaSyAWp0XyrtpVfw0HUbCQdr-Jzl-2yyqpHiI";
-    const query = "subject:Business";
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}&printType=books&startIndex=0&maxResults=6&langRestrict=en`;
+    //const query = "subject:Business";
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q="subject:Business"&key=${apiKey}&printType=books&startIndex=0&maxResults=6&langRestrict=en`;
 
     fetch(apiUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('Ошибка связи');
             }
             return response.json();
         })
@@ -80,32 +64,41 @@ function fetchBooks() {
                 const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '';
                 const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : '';
                 const title = book.volumeInfo.title;
-                const rate = book.volumeInfo.rate ? book.volumeInfo.rate.thumbnail : '';
+                const ratingsCount = book.volumeInfo.ratingsCount ? book.volumeInfo.ratingsCount.thumbnail : '';
                 const description = book.volumeInfo.description ? book.volumeInfo.description : '';
-                
-
-                
+                const retailPrice = book.volumeInfo.retailPrice;
+                                
                 bookElement.innerHTML = `
                     <img src="${thumbnail}" alt="Thumbnail">
                     <div class="book-info">
-                    <p>Authors: ${authors}</p>
+                    <p>${authors}</p>
                     <h2>${title}</h2>
-                    <h3>${rate}</h3>
-                    <p>Description: ${description}</p>
+                    <h3>${ratingsCount}</h3>
+                    <p>${description}</p>
+                    <p>${retailPrice}</p>
                     </div>
                 `;
-
-           
+          
                 booksContainer.appendChild(bookElement);
             });
         })
         .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
+            console.error('Ошибка:', error);
         });
 }
 
-const button = document.getElementById('button');
 
-button.addEventListener('click', function() {
-    fetchBooks(); 
+
+document.querySelectorAll('.category').forEach(item => {
+    item.addEventListener('click', function() {
+        document.querySelectorAll('.category').forEach(item => {
+            item.classList.remove('sidebar-active');
+            item.classList.add('sidebar-txt');
+            fetchBooks(); 
+        });
+        this.classList.remove('sidebar-txt');
+        this.classList.add('sidebar-active');
+    });
 });
+
+
